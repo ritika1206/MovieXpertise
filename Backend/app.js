48d88c9dc84889f2eab3a136f423fdf02b4c904a
ritika1206 <67223899+ritika1206@users.noreply.github.com>
2022-04-11 21:08:31 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -37,7 +37,7 @@ app.use((req, res, next) => {
     res.json({ message: error.message || 'An unknown error occurred!' });
   });
 
-uri = "mongodb+srv://ritika1234:08414802718@users.h6wod.mongodb.net/MovieXpertise?retryWrites=true&w=majority";
+uri = "";
 
 mongoose
   .connect(uri)
